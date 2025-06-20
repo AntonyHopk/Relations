afee22934b09a9c0a907a0697fd1f46efe1c3cf1
AntonyHopk <125564689+AntonyHopk@users.noreply.github.com>
2025-06-20 20:59:55 +0300

    Create auth entity

---
+++ src/main/java/org/example/relations/entity/Credentials.java
@@ -0,0 +1,31 @@
+package org.example.relations.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(schema = "users_schema", name = "t_credentials")
+public class Credentials {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(nullable = false, name = "c_username")
+    String username;
+    @Column(nullable = false, name = "c_password")
+    String password;
+
+    @ManyToOne
+    @JoinColumn(name = "c_role_id")
+    Role role;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "c_user_id", referencedColumnName = "id")
+    User user;
+}
