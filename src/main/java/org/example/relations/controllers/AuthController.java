5efac0a9b46d5770e8537079757f4ee474b4cb0a
AntonyHopk <125564689+AntonyHopk@users.noreply.github.com>
2025-06-26 19:21:21 +0300

    bug fixes

--- src/main/java/org/example/relations/controllers/AuthController.java
+++ src/main/java/org/example/relations/controllers/AuthController.java
@@ -1,5 +1,8 @@
 package org.example.relations.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.relations.DTO.RegisterRequest;
 import org.example.relations.services.AuthService;
@@ -10,15 +13,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@Tag(name = "Auth API", description = "Auth аккаунта")
+
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(summary = "Register",  description = "Register now ", responses = {
+            @ApiResponse(responseCode = "200", description = "Register successfully")
+    })
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
         authService.register(registerRequest);
         return ResponseEntity.ok("Register was successfully");
     }
-
+    @Operation(summary = "Login",  description = "Login for what?", responses = {
+            @ApiResponse(responseCode = "200", description = "Login successfully")
+    })
     @GetMapping("/login")
     public ResponseEntity<?> login(@RequestBody Authentication authentication) {
         return ResponseEntity.ok("Login was successful " + authentication.getName());
